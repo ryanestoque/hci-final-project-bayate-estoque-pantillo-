0da179ce492f1f69f29b08ab9a59d10df6da5022
ryanestoque <166630170+ryanestoque@users.noreply.github.com>
2024-05-06 23:09:08 +0800

    Add files via upload

---
+++ scripts/overlay.js
@@ -0,0 +1,59 @@
+function showSinuglawOverlay() {
+  document.getElementById("sinuglaw-overlay").style.display = "block";
+  document.getElementById("overlay-background").style.display = "block";
+}
+
+function hideSinuglawOverlay() {
+  document.getElementById("sinuglaw-overlay").style.display = "none";
+  document.getElementById("overlay-background").style.display = "none";
+}
+
+function showPorkSiomaiOverlay() {
+  document.getElementById("pork-siomai-overlay").style.display = "block";
+  document.getElementById("overlay-background").style.display = "block";
+}
+
+function hidePorkSiomaiOverlay() {
+  document.getElementById("pork-siomai-overlay").style.display = "none";
+  document.getElementById("overlay-background").style.display = "none";
+}
+
+function showMangoShakeOverlay() {
+  document.getElementById("mango-shake-overlay").style.display = "block";
+  document.getElementById("overlay-background").style.display = "block";
+}
+
+function hideMangoShakeOverlay() {
+  document.getElementById("mango-shake-overlay").style.display = "none";
+  document.getElementById("overlay-background").style.display = "none";
+}
+
+function showSiopaoOverlay() {
+  document.getElementById("siopao-overlay").style.display = "block";
+  document.getElementById("overlay-background").style.display = "block";
+}
+
+function hideSiopaoOverlay() {
+  document.getElementById("siopao-overlay").style.display = "none";
+  document.getElementById("overlay-background").style.display = "none";
+}
+
+function showTapsilogOverlay() {
+  document.getElementById("tapsilog-overlay").style.display = "block";
+  document.getElementById("overlay-background").style.display = "block";
+}
+
+function hideTapsilogOverlay() {
+  document.getElementById("tapsilog-overlay").style.display = "none";
+  document.getElementById("overlay-background").style.display = "none";
+}
+
+function showFrenchFriesOverlay() {
+  document.getElementById("french-fries-overlay").style.display = "block";
+  document.getElementById("overlay-background").style.display = "block";
+}
+
+function hideFrenchFriesOverlay() {
+  document.getElementById("french-fries-overlay").style.display = "none";
+  document.getElementById("overlay-background").style.display = "none";
+}
